403311a53fa8d265fb714c9e8d631d0da9849b42
Ali Tunç <tunchalikali@gmail.com>
2023-02-25 16:55:41 +0300

    set up storybook

--- src/components/UI/atoms/Button/Button.tsx
+++ src/components/UI/atoms/Button/Button.tsx
@@ -1,5 +1,24 @@
-function Button() {
-  return <div>Button</div>
+interface ButtonProps {
+  primary?: boolean
+  backgroundColor?: string
+  size?: 'small' | 'medium' | 'large'
+  label: string
+  onClick?: () => void
 }
 
-export default Button
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = ({ primary = false, size = 'medium', backgroundColor, label, ...props }: ButtonProps) => {
+  const mode = primary ? 'button--primary' : 'button--secondary'
+  return (
+    <button
+      type="button"
+      className={['button', `button--${size}`, mode].join(' ')}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
+    </button>
+  )
+}
