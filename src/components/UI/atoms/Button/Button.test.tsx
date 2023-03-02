f1189c21820fc91b4e6090409ef5ab3e10b29536
Ali Tunç <tunchalikali@gmail.com>
2023-03-02 22:45:55 +0300

    test and code button

--- src/components/UI/atoms/Button/Button.test.tsx
+++ src/components/UI/atoms/Button/Button.test.tsx
@@ -1,10 +1,50 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { describe, it } from 'vitest'
 import Button from './Button'
 
 describe('Button', () => {
   it('renders a button', () => {
-    const { getByRole } = render(<Button label="button" />)
+    const { getByRole } = render(<Button size="large" label="button" />)
     expect(getByRole('button')).toBeInTheDocument()
+  })
+
+  it('renders the correct label', () => {
+    render(<Button size="large" label="Example Label" />)
+    const renderedLabel = screen.getByText('Example Label')
+    expect(renderedLabel).toBeInTheDocument()
+  })
+
+  it('renders the correct size', () => {
+    render(<Button size="large" label="button" />)
+    const renderedButton = screen.getByRole('button')
+    expect(renderedButton).toHaveClass('button--large')
+  })
+
+  it('renders the correct mode', () => {
+    render(<Button size="large" label="button" mode="with-icon" />)
+    const renderedButton = screen.getByRole('button')
+    expect(renderedButton).toHaveClass('button--with-icon')
+  })
+
+  it('renders the correct priority', () => {
+    render(<Button size="large" label="button" priority="secondary" />)
+    const renderedButton = screen.getByRole('button')
+    expect(renderedButton).toHaveClass('button--secondary')
+  })
+
+  it('renders the correct icon', () => {
+    render(
+      <Button size="large" label="button" mode="with-icon">
+        <span>icon</span>
+      </Button>
+    )
+    const renderedButton = screen.getByRole('button')
+    expect(renderedButton).toHaveTextContent('icon')
+  })
+
+  it('renders a button without a label', () => {
+    render(<Button size="large" label="Example Label" mode="just-icon" />)
+    const renderedButton = screen.getByRole('button')
+    expect(renderedButton).toHaveTextContent('')
   })
 })
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,26 +1,7 @@
-import { useTranslation } from 'react-i18next'
-
-import Button from 'components/UI/atoms/Button/Button'
-import { AddIcon, TrashIcon } from 'assets'
-
 function Home() {
-  const { t } = useTranslation()
-
   return (
     <div>
-      <p>hello world</p>
-      <h1>example heading</h1>
-      <Button label={t('general.add')} size="large" mode="with-icon">
-        <img src={AddIcon} alt="add icon" />
-      </Button>
-
-      <Button label={t('general.add')} size="medium" mode="with-icon">
-        <img src={AddIcon} alt="add icon" />
-      </Button>
-
-      <Button label={t('general.add')} mode="just-icon" size="small" priority="secondary">
-        <img src={TrashIcon} alt="add icon" className="button__image" />
-      </Button>
+      <h1>Home</h1>
     </div>
   )
 }
